7c76c076928c4bc24b8ed1f3e4cc1b48093cd73e
Vania Godoy <vaniafatima@msn.com>
2026-03-31 19:47:36 -0300

    configuração da API e Retrofit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiService.kt
@@ -0,0 +1,26 @@
+package br.edu.ifsp.scl.sdm.currencyconverter.model.api
+
+import br.edu.ifsp.scl.sdm.currencyconverter.model.domain.ConversionResult
+import br.edu.ifsp.scl.sdm.currencyconverter.model.domain.CurrencyList
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface CurrencyConverterApiService {
+
+  @Headers(
+    "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+    "x-rapidapi-key: 123456"
+  )
+
+  @GET("list")
+  fun getCurrencies(): Call<CurrencyList>
+
+  @GET("convert")
+  fun convert(
+    @Query("from") from: String,
+    @Query ("to") to: String,
+    @Query ("amount") amount: String
+    ): Call<ConversionResult>
+}
